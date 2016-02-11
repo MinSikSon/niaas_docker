b543c29b52887816106deac67049b4e89b15b812
Son, Minsik <tofu89223@gmail.com>
2016-02-11 15:19:06 +0900

    edit entrypoint.sh

--- fog_global/entrypoint.sh
+++ fog_global/entrypoint.sh
@@ -5,14 +5,14 @@ sed -i "s/bind-address		= 127.0.0.1/# bind-address		= 127.0.0.1/g" /etc/mysql/my
 mysql -uroot -Dmysql -e"use mysql;"
 echo "use mysql"
 #mysql -uroot -Dmysql -e"select host,user from user;"
-mysql -uroot -Dmysql -e"update user set password=password(\"dir7413\") where user='root';"
-echo "update user set password=password(\"dir7413\") where user='root';"
+mysql -uroot -Dmysql -e"update user set password=password(\"<your password>\") where user='root';"
+echo "update user set password=password(\"<your password>\") where user='root';"
 mysql -uroot -Dmysql -e"flush privileges;"
 echo "flush privileges;"
 /etc/init.d/mysql restart
 # admit join in mysql from remote;
 mysql -uroot -pdir7413 -e"INSERT INTO mysql.user (host,USER,password) VALUES ('%','root',password('dir7413'));"
-echo "INSERT INTO mysql.user (host,USER,password) VALUES ('%','root',password('dir7413'));"
+echo "INSERT INTO mysql.user (host,USER,password) VALUES ('%','root',password('<your password>'));"
 mysql -uroot -pdir7413 -e"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
 mysql -uroot -pdir7413 -e"FLUSH PRIVILEGES;"
